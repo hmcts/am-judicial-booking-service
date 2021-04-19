fb0a5c34fa493044010f068832c9f0752c31e2de
Nitin <nitin.sahu@hmcts.net>
2021-04-19 15:25:57 +0100

    AM-1316:Added Fly scripts and clean up of code

--- src/main/java/uk/gov/hmcts/reform/judicialbooking/JudicialBookingApplication.java
+++ src/main/java/uk/gov/hmcts/reform/judicialbooking/JudicialBookingApplication.java
@@ -6,17 +6,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
+
+import java.time.Clock;
+
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableFeignClients
 @ConfigurationProperties
+@EnableCaching
+@EnableFeignClients(basePackages = {
+        "uk.gov.hmcts.reform.judicialbooking"}, basePackageClasses = {IdamApi.class, ServiceAuthorisationApi.class})
 public class JudicialBookingApplication {
 
     public static void main(final String[] args) {
@@ -26,16 +33,22 @@ public class JudicialBookingApplication {
     @Bean
     public ServiceAuthorisationApi generateServiceAuthorisationApi(@Value("${idam.s2s-auth.url}") final String s2sUrl) {
         return Feign.builder()
-                    .encoder(new JacksonEncoder())
-                    .contract(new SpringMvcContract())
-                    .target(ServiceAuthorisationApi.class, s2sUrl);
+                .encoder(new JacksonEncoder())
+                .contract(new SpringMvcContract())
+                .target(ServiceAuthorisationApi.class, s2sUrl);
     }
 
     @Bean
     public ServiceAuthTokenGenerator authTokenGenerator(
-        @Value("${idam.s2s-auth.totp_secret}") final String secret,
-        @Value("${idam.s2s-auth.microservice}") final String microService,
-        final ServiceAuthorisationApi serviceAuthorisationApi) {
+            @Value("${idam.s2s-auth.totp_secret}") final String secret,
+            @Value("${idam.s2s-auth.microservice}") final String microService,
+            final ServiceAuthorisationApi serviceAuthorisationApi) {
         return new ServiceAuthTokenGenerator(secret, microService, serviceAuthorisationApi);
     }
+
+    @Bean
+    public Clock utcClock() {
+        return Clock.systemUTC();
+    }
+
 }
