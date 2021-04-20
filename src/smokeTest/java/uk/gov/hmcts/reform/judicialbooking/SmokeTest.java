9eec5a862195a32075386192d5bb2fac894486de
Nitin <nitin.sahu@hmcts.net>
2021-04-20 15:59:30 +0100

    AM-1316: Fixed smoke tests

--- src/smokeTest/java/uk/gov/hmcts/reform/judicialbooking/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/judicialbooking/SmokeTest.java
@@ -2,31 +2,41 @@ package uk.gov.hmcts.reform.judicialbooking;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lombok.NoArgsConstructor;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
+import org.apache.commons.lang3.Validate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class SmokeTest {
+@RunWith(SpringIntegrationSerenityRunner.class)
+@NoArgsConstructor
+@WithTags({@WithTag("testType:Smoke")})
+public class SmokeTest extends BaseTest {
 
     @Value("${judicialBookingUrl}")
     private String baseUrl;
 
     @Test
-    public void shouldGetHealthStatus() {
-        RestAssured.baseURI = baseUrl;
+    public void should_receive_response_for_welcomeAPI() {
+
+        String targetInstance = getRequiredVariable("TEST_URL") + "/welcome";
         RestAssured.useRelaxedHTTPSValidation();
+
         Response response = SerenityRest
                 .given()
                 .relaxedHTTPSValidation()
                 .when()
-                .get("/health")
+                .get(targetInstance)
                 .andReturn();
         response.then().assertThat().statusCode(HttpStatus.OK.value());
+    }
+
+    public static String getRequiredVariable(String name) {
+        return Validate.notNull(System.getenv(name), "Environment variable `%s` is required", name);
     }
 }
