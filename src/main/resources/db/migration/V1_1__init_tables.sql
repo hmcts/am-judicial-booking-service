91612f97c95e70815da745cc88c75053cd753f03
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2021-04-20 18:56:16 +0100

    added the booking table

--- src/main/resources/db/migration/V1_1__init_tables.sql
+++ src/main/resources/db/migration/V1_1__init_tables.sql
@@ -1,6 +1,19 @@
-CREATE TABLE actor_cache_control(
-	actor_id text NOT NULL,
-	etag int4 NOT NULL,
-	json_response jsonb NOT NULL,
-	CONSTRAINT actor_cache_control_pkey PRIMARY KEY (actor_id)
-); 
+CREATE TABLE booking(
+	id bigint NOT NULL,
+	user_id text NOT NULL,
+	appointment_id text NOT NULL,
+	role_id text NOT NULL,
+	contract_type_id text NOT NULL,
+	base_location_id text,
+	region_id text,
+	status text NOT NULL,
+	begin_time timestamp NOT NULL,
+	end_time timestamp NOT NULL,
+	created timestamp NOT NULL,
+	log text,
+	CONSTRAINT booking_pkey PRIMARY KEY (id)
+);
+
+create sequence BOOKING_ID_SEQ;
+ALTER TABLE booking ALTER COLUMN id
+SET DEFAULT nextval('BOOKING_ID_SEQ');
