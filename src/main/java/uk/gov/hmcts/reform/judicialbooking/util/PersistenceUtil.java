9e4fb7ca2302ea1bdbe83e352dca01022ce5c608
ConorH994 <conor.halpin@hmcts.net>
2021-04-29 22:18:20 +0100

    forgot to add the simple persistence util for converting between booking and booking entity

---
+++ src/main/java/uk/gov/hmcts/reform/judicialbooking/util/PersistenceUtil.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.judicialbooking.util;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.judicialbooking.data.BookingEntity;
+import uk.gov.hmcts.reform.judicialbooking.domain.model.Booking;
+
+@Service
+public class PersistenceUtil {
+
+    public BookingEntity convertBookingToEntity(Booking booking) {
+        return BookingEntity.builder()
+                .userId(booking.getUserId())
+                .appointmentId(booking.getAppointmentId())
+                .baseLocationId(booking.getBaseLocationId())
+                .roleId(booking.getRoleId())
+                .regionId(booking.getRegionId())
+                .contractTypeId(booking.getContractTypeId())
+                .beginTime(booking.getBeginTime())
+                .endTime(booking.getEndTime())
+                .created(booking.getCreated())
+                .status(booking.getStatus())
+                .log(booking.getLog())
+                .build();
+    }
+
+    public Booking convertBookingEntityToBooking(BookingEntity bookingEntity) {
+        return Booking.builder()
+                .userId(bookingEntity.getUserId())
+                .appointmentId(bookingEntity.getAppointmentId())
+                .baseLocationId(bookingEntity.getBaseLocationId())
+                .roleId(bookingEntity.getRoleId())
+                .regionId(bookingEntity.getRegionId())
+                .contractTypeId(bookingEntity.getContractTypeId())
+                .beginTime(bookingEntity.getBeginTime())
+                .endTime(bookingEntity.getEndTime())
+                .created(bookingEntity.getCreated())
+                .status(bookingEntity.getStatus())
+                .log(bookingEntity.getLog())
+                .build();
+    }
+}
