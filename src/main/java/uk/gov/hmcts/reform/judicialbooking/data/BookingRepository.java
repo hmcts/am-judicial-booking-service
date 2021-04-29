ba5fdb563fa755714b48aa707dc7a2632f446834
ConorH994 <conor.halpin@hmcts.net>
2021-04-29 16:58:31 +0100

    added repository interface

---
+++ src/main/java/uk/gov/hmcts/reform/judicialbooking/data/BookingRepository.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.judicialbooking.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookingRepository extends CrudRepository<BookingEntity, UUID> {
+}
