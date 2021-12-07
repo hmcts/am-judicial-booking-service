06b7a9d82e8f4d34067d6b9b9f59af4948a2db5f
Nitish Singla <nitish.singla@hmcts.net>
2021-12-07 09:50:11 +0000

    Uncommenting the integration test.

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/WelcomeControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/WelcomeControllerIntegrationTest.java
@@ -1,53 +1,53 @@
-//package uk.gov.hmcts.reform.judicialbooking.controller;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//
-//import java.nio.charset.Charset;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-//
-//
-//public class WelcomeControllerIntegrationTest extends BaseTest {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(WelcomeControllerIntegrationTest.class);
-//
-//    private transient MockMvc mockMvc;
-//
-//    private static final MediaType JSON_CONTENT_TYPE = new MediaType(
-//        MediaType.APPLICATION_JSON.getType(),
-//        MediaType.APPLICATION_JSON.getSubtype(),
-//        Charset.forName("utf8")
-//    );
-//
-//    @Autowired
-//    private transient WelcomeController welcomeController;
-//
-//    @Before
-//    public void setUp() {
-//        this.mockMvc = standaloneSetup(this.welcomeController).build();
-//    }
-//
-//    @Test
-//    public void welcomeApiTest() throws Exception {
-//        final String url = "/welcome";
-//        logger.info(" WelcomeControllerIntegrationTest : Inside  Welcome API Test method...{}", url);
-//        final MvcResult result = mockMvc.perform(get(url).contentType(JSON_CONTENT_TYPE))
-//                .andExpect(status().is(200))
-//                .andReturn();
-//        assertEquals(
-//                "Welcome service status", 200, result.getResponse().getStatus());
-//        assertEquals(
-//                "Welcome service message", "Welcome to Judicial Booking service",
-//                result.getResponse().getContentAsString());
-//    }
-//}
+package uk.gov.hmcts.reform.judicialbooking.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+
+public class WelcomeControllerIntegrationTest extends BaseTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(WelcomeControllerIntegrationTest.class);
+
+    private transient MockMvc mockMvc;
+
+    private static final MediaType JSON_CONTENT_TYPE = new MediaType(
+        MediaType.APPLICATION_JSON.getType(),
+        MediaType.APPLICATION_JSON.getSubtype(),
+        Charset.forName("utf8")
+    );
+
+    @Autowired
+    private transient WelcomeController welcomeController;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = standaloneSetup(this.welcomeController).build();
+    }
+
+    @Test
+    public void welcomeApiTest() throws Exception {
+        final String url = "/welcome";
+        logger.info(" WelcomeControllerIntegrationTest : Inside  Welcome API Test method...{}", url);
+        final MvcResult result = mockMvc.perform(get(url).contentType(JSON_CONTENT_TYPE))
+                .andExpect(status().is(200))
+                .andReturn();
+        assertEquals(
+                "Welcome service status", 200, result.getResponse().getStatus());
+        assertEquals(
+                "Welcome service message", "Welcome to Judicial Booking service",
+                result.getResponse().getContentAsString());
+    }
+}
