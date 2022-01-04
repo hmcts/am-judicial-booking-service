2aa0cf27105620f4a93c62b226ce0890f6d05f85
shivajilokare <shivaji.lokare@HCTS.NET>
2022-01-04 11:42:39 +0000

    Adding  test cases

---
+++ src/test/java/uk/gov/hmcts/reform/judicialbooking/util/ApplicationUtilsTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.judicialbooking.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationUtilsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void generateHashCode() {
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/judicialbooking/util/Swagger2SpringBootTest.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.judicialbooking.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Swagger2SpringBootTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void run() {
+    }
+}
