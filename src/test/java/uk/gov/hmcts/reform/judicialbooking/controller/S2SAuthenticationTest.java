015859c9b69fe3067986d52df1bfb02293dda0e0
shivajilokare <shivaji.lokare@HCTS.NET>
2022-01-04 11:35:23 +0000

    Adding  test cases

---
+++ src/test/java/uk/gov/hmcts/reform/judicialbooking/controller/S2SAuthenticationTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.judicialbooking.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class S2SAuthenticationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testS2SAuthorization() {
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/judicialbooking/controller/WelcomeControllerTest.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.judicialbooking.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WelcomeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void welcome() {
+    }
+
+    @Test
+    void deleteBookingByUserId() {
+    }
+}
