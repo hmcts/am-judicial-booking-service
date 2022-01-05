18fcf8bae081b4c92f872b8c2b08b22c81273b42
shivajilokare <shivaji.lokare@HCTS.NET>
2022-01-05 16:56:51 +0000

    checkstyle issues fixed

--- src/test/java/uk/gov/hmcts/reform/judicialbooking/controller/S2SAuthenticationTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialbooking/controller/S2SAuthenticationTest.java
@@ -1,20 +1,9 @@
 package uk.gov.hmcts.reform.judicialbooking.controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class S2SAuthenticationTest {
 
