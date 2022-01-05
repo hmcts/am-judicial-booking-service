241ba87ba082ac668336cf8683986d53a98b8874
shivajilokare <shivaji.lokare@HCTS.NET>
2022-01-05 18:27:00 +0000

    checkstyle issues fixed

--- src/test/java/uk/gov/hmcts/reform/judicialbooking/domain/service/common/ParseRequestServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialbooking/domain/service/common/ParseRequestServiceTest.java
@@ -100,7 +100,7 @@ class ParseRequestServiceTest {
 
     @Test
     void testParseBookingValidateRequest() {
-        Assertions.assertThrows(BadRequestException.class,
-                () -> sut.parseBookingRequest(TestDataBuilder.buildWrongBooking()));
+        BookingRequest bookingRequest = TestDataBuilder.buildWrongBooking();
+        Assertions.assertThrows(BadRequestException.class, () -> sut.parseBookingRequest(bookingRequest));
     }
 }
