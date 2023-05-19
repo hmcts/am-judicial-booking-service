76448f22ed2c1110f7f1f42b6bae49049eda793e
Mike Brown <michael.brown@hmcts.net>
2023-05-19 17:24:14 +0100

    SwaggerPublisher output file updated to openapi-specs.json

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/config/SwaggerPublisher.java
@@ -44,7 +44,7 @@ public class SwaggerPublisher extends BaseTest {
             .getResponse()
             .getContentAsByteArray();
 
-        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/openapi-specs.json"))) {
             outputStream.write(specs);
         }
     }
