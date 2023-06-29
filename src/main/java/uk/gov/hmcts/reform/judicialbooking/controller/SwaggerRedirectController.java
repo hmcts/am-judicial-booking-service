2cf8b36b7ce252d1b29d0c66bb052f15cc0e2c35
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-06-29 15:29:27 +0100

    AM-2849: Retire obsolete controllers and associated tests after review

---
+++ src/main/java/uk/gov/hmcts/reform/judicialbooking/controller/SwaggerRedirectController.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.judicialbooking.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static org.springdoc.core.Constants.SWAGGER_UI_URL;
+
+@RestController
+@Hidden
+public class SwaggerRedirectController {
+
+    @GetMapping(value = "/swagger")
+    public RedirectView swaggerRedirect() {
+        return new RedirectView(SWAGGER_UI_URL, true, false);
+    }
+}
