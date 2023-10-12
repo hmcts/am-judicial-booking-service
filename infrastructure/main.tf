dca8aee87d30539aa3d79504254553be4817a9e4
Mike Brown <michael.brown@hmcts.net>
2023-10-12 16:12:30 +0100

    AM-2947 - db owner added to provision

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,7 +80,8 @@ module "judicial-booking-database-v15" {
 
   pgsql_databases = [
       {
-        name = var.database_name
+        name    = var.database_name
+        owner   = var.postgresql_user
       }
     ]
 }
