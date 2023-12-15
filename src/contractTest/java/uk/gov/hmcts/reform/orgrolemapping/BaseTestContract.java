4911aa104f428228066d76fd8bbad602438453eb
Mike Brown <michael.brown@hmcts.net>
2023-12-15 13:37:54 +0000

    unused imports removed

--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/BaseTestContract.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/BaseTestContract.java
@@ -1,22 +1,7 @@
 package uk.gov.hmcts.reform.orgrolemapping;
 
-
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.judicialbooking.controller.BaseTest;
-
-import javax.annotation.PreDestroy;
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
 
 @ActiveProfiles("ctest")
 
