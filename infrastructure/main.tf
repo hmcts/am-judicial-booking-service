bc412c3709f78cc523d6afe7cd62a8793ecd82b3
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2024-05-02 22:59:17 +0100

    AM-3059 Reader User Trigger 7 Prod (#1044)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -84,7 +84,7 @@ module "judicial-booking-database-v15" {
   pgsql_version      = "15"
 
   # Setup Access Reader db user
-  force_user_permissions_trigger = "6"
+  force_user_permissions_trigger = "7"
 
   # Sets correct DB owner after migration to fix permissions
   enable_schema_ownership = var.enable_schema_ownership
