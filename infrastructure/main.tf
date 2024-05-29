c44f76e2a47602e12759b264e49d7603b6b0b230
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2024-05-29 17:13:53 +0100

    DTSAM-321 Decommission custom Postgres-V15 key vault values now that the standard ones are pointing to V15 instance (#1074)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -116,21 +116,3 @@ module "judicial-booking-database-v15" {
     }
   ]
 }
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS-V15" {
-  name          = "${var.component}-POSTGRES-PASS-V15"
-  value         = module.judicial-booking-database-v15.password
-  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-USER-V15" {
-  name         = "${var.component}-POSTGRES-USER-V15"
-  value        = module.judicial-booking-database-v15.username
-  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-HOST-V15" {
-  name         = "${var.component}-POSTGRES-HOST-V15"
-  value        = module.judicial-booking-database-v15.fqdn
-  key_vault_id  = data.azurerm_key_vault.am_key_vault.id
-}
