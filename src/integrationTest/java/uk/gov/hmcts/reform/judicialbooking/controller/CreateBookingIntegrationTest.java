906d2edcd1f3cb7bcf41a6bfd1e2f7b27c97f7d5
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-01-06 11:08:08 +0000

    DTSAM-1158 Remove the redundant mockUtils.

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/CreateBookingIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/CreateBookingIntegrationTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.judicialbooking.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.parsing.Parser;
-import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.rest.SerenityRest;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +20,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.judicialbooking.client.IdamOpenId;
 import uk.gov.hmcts.reform.judicialbooking.client.S2sClient;
 import uk.gov.hmcts.reform.judicialbooking.config.TestConfigProperties;
-import uk.gov.hmcts.reform.judicialbooking.controller.utils.MockUtils;
 import uk.gov.hmcts.reform.judicialbooking.domain.model.BookingRequest;
 import uk.gov.hmcts.reform.judicialbooking.domain.model.BookingRequestWrapper;
 
@@ -81,15 +79,10 @@ public class CreateBookingIntegrationTest {
 
     @Test
     public void rejectRequestWithoutBody() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(SERVICE_HEADER, "Bearer " + s2sToken);
-        headers.add(AUTHORIZATION_HEADER, "Bearer " + idamOpenIdClient.getcwdAdminOpenIdToken( "caseworker-civil-admin"));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
         SerenityRest.with()
                 .relaxedHTTPSValidation()
                 .baseUri(baseUrl)
-                .headers(headers)
+                .headers(getHeaders())
                 .when().post(URL)
                 .then().assertThat()
                 .statusCode(HttpStatus.BAD_REQUEST.value())
@@ -102,8 +95,9 @@ public class CreateBookingIntegrationTest {
         var request = new BookingRequestWrapper(new BookingRequest(null, null, LOCATION, LocalDate.now(),
                 LocalDate.now()));
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(request))
                 .when().post(URL)
                 .then().assertThat()
@@ -117,8 +111,9 @@ public class CreateBookingIntegrationTest {
         var request = new BookingRequestWrapper(new BookingRequest(null, REGION, LOCATION, null,
                 LocalDate.now()));
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(request))
                 .when().post(URL)
                 .then().assertThat()
@@ -132,8 +127,9 @@ public class CreateBookingIntegrationTest {
         var request = new BookingRequestWrapper(new BookingRequest(null, REGION, LOCATION, LocalDate.now(),
                 null));
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(request))
                 .when().post(URL)
                 .then().assertThat()
@@ -196,8 +192,9 @@ public class CreateBookingIntegrationTest {
                 LocalDate.now().minusDays(1));
 
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(new BookingRequestWrapper(request)))
                 .when().post(URL)
                 .then().assertThat()
@@ -214,8 +211,9 @@ public class CreateBookingIntegrationTest {
                 LocalDate.now().plusDays(5), LocalDate.now());
 
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(new BookingRequestWrapper(request)))
                 .when().post(URL)
                 .then().assertThat()
@@ -232,8 +230,9 @@ public class CreateBookingIntegrationTest {
                 LocalDate.now().minusDays(5), LocalDate.now().minusDays(1));
 
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(new BookingRequestWrapper(request)))
                 .when().post(URL)
                 .then().assertThat()
@@ -274,11 +273,20 @@ public class CreateBookingIntegrationTest {
                 LocalDate.now(), LocalDate.now().plusDays(1)));
 
         SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .headers(MockUtils.getHttpHeaders(MockUtils.S2S_JBS))
+                .relaxedHTTPSValidation()
+                .baseUri(baseUrl)
+                .headers(getHeaders())
                 .body(mapper.writeValueAsBytes(request))
                 .when().post(URL)
                 .then().assertThat()
                 .statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value());
+    }
+    
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(SERVICE_HEADER, "Bearer " + s2sToken);
+        headers.add(AUTHORIZATION_HEADER, "Bearer " + idamOpenIdClient.getcwdAdminOpenIdToken( "caseworker-civil-admin"));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }
