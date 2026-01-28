6eeb70a0d0fe549f71d1c13b8d0075ca79c07e34
Mark Harris <156429052+markharriscgi@users.noreply.github.com>
2026-01-28 14:32:09 +0000

    DTSAM-1158 Introduce Serenity for the integration tests (#1390)
    * DTSAM-1158 Swap mockMvc for SerenityRest
    
    * DTSAM-1158 Add more serenity tests.
    
    * DTSAM-1158 Swap BAD_REQUEST to OK, to test serenity failure in pipeline.
    
    * DTSAM-1158 Introduce serenityRest.
    
    * DTSAM-1158 Add missing changes.
    
    * DTSAM-1158 Add the Idam classes.
    
    * DTSAM-1158 Remove the redundant mockUtils.
    
    * DTSAM-1158 Remove the profile annotation.
    
    * DTSAM-1158 Remove the wiremock ports.
    
    * DTSAM-1158 Create a separate serenity application yaml.
    
    * DTSAM-1158 Make the testconfigproperties only reference serenity application.yaml.
    
    * DTSAM-1158 Add a default value for oauth2-client-secret
    
    * DTSAM-1158 Put the  on the wiremocks back in.
    
    * DTSAM-1158 Trim the code back to basics.
    
    * DTSAM-1158 Add the userAuthToken.
    
    * DTSAM-1158 Add the pipeline copy of reports, even if the pipeline fails.
    
    * DTSAM-1158 Remove the redundant build entires.
    
    * DTSAM-1158 Reintroduce the commented tests.
    
    * DTSAM-1158 Add Serernity report to the jenkins build.
    
    * DTSAM-1158 Introduce serenity.properties
    
    * DTSAM-1158 Stop the Junit and Serenity clashing.
    
    * DTSAM-1158 Remove unused imports.
    
    * DTSAM-1158 Readd depends on
    
    * DTSAM-1158 Delete serenity.properties
    
    * DTSAM-1158 Republish Serentiy Report after Smoke tests
    
    * DTSAM-1158 Republish Serentiy Report after Smoke tests
    
    * DTSAM-1158 Don't include raw Serenity and Cucumber report files in artifacts as too many to navigate
    
    * DTSAM-1158 try alternative way to send JUnit artifacts
    
    * DTSAM-1158 Add the serenity changes to delete and query.
    
    * DTSAM-1158 Centralise the mapper.
    
    * DTSAM-1158 Add the calls to WIRE_MOCK_SERVER with the correct serviceName and actorId.  Currently fails.
    
    * DTSAM-1158 Add new missing class.
    
    * DTSAM-1158 Resolve retrieveJudicialBookingsInvalidUser_bypassValidation
    
    * DTSAM-1158 Tidy up the codebase.
    
    * DTSAM-1158 Add a delay to allow the wiremockserver reset to catch up.
    
    * DTSAM-1158 Final code review.
    
    * DTSAM-1158 Add serenity report to nightly build.  Make sure the serenity applies to swagger tests too, to prevent overwrite.
    
    * DTSAM-1158 Resolve the servicename in the header and make sure the bypass works correctly.
    
    * DTSAM-1158 Stop the serenity report appearing twice (only appears on success for preview and aat).
    
    * DTSAM-1158 Reintroduce the serenity report publish (if failures happen then no report would have be shown)
    
    ---------
    
    Co-authored-by: Matt Nayler <57350764+mattnayler@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/DeleteBookingControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/DeleteBookingControllerIntegrationTest.java
@@ -1,45 +1,18 @@
 package uk.gov.hmcts.reform.judicialbooking.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.reform.judicialbooking.controller.endpoints.DeleteBookingController;
+import org.springframework.http.HttpStatus;
 
-import java.nio.charset.StandardCharsets;
+import static uk.gov.hmcts.reform.judicialbooking.controller.utils.WiremockFixtures.ACTOR_ID1;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
-public class DeleteBookingControllerIntegrationTest extends BaseTestIntegration {
-
-    private transient MockMvc mockMvc;
-
-    private static final MediaType JSON_CONTENT_TYPE = new MediaType(
-        MediaType.APPLICATION_JSON.getType(),
-        MediaType.APPLICATION_JSON.getSubtype(),
-        StandardCharsets.UTF_8
-    );
-
-    @Autowired
-    private transient DeleteBookingController deleteBookingController;
-
-    @BeforeEach
-    public void setUp() {
-        this.mockMvc = standaloneSetup(this.deleteBookingController).build();
-    }
+public class DeleteBookingControllerIntegrationTest extends BaseAuthorisedTestIntegration {
+    private static final String URL = "/am/bookings/";
 
     @Test
     public void deleteBookingByUserIdApiTest() throws Exception {
-        final var url = "/am/bookings/";
-        final var userId = "631d322c-eea7-4d53-bd92-e6ec51bcb390";
-
-        mockMvc.perform(delete(url + userId)
-                .contentType(JSON_CONTENT_TYPE)
-                .headers(getHttpHeaders()))
-                .andExpect(status().isNoContent())
-                .andReturn();
+        getRequestSpecification()
+                .when().delete(String.format("%s%s", URL, ACTOR_ID1))
+                .then().assertThat()
+                .statusCode(HttpStatus.NO_CONTENT.value());
     }
 }
