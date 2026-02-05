16694ae7599df37adea6c0755edc50fe538a7e66
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2026-02-05 16:25:45 +0000

    checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/CreateBookingIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/CreateBookingIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.http.HttpStatus;
