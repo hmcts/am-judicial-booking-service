94902ba889c8e3a96f52d3bb993bfd869d76d5ef
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-02-26 08:27:16 +0000

    DTSAM-1209 Add env variable.

--- src/contractTest/java/uk/gov/hmcts/reform/judicialbooking/QueryBookingProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/judicialbooking/QueryBookingProviderTest.java
@@ -31,7 +31,7 @@ import static org.mockito.ArgumentMatchers.any;
         host = "${PACT_BROKER_URL:localhost}", port = "${PACT_BROKER_PORT:9292}",
         consumerVersionSelectors = {@VersionSelector(tag = "master")})
 @TestPropertySource(properties = {"spring.cache.type=none",
-        "judicial-booking.query.bypass-userid-validation-for-services=am_org_role_mapping_service"})
+    "judicial-booking.query.bypass-userid-validation-for-services=am_org_role_mapping_service"})
 @Import(ProviderTestConfiguration.class)
 @IgnoreNoPactsToVerify
 public class QueryBookingProviderTest {
