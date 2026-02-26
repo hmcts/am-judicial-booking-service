c97be270bcd8efd9c453f461add253849f6caf0e
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-02-26 10:29:10 +0000

    DTSAM-1210 Set the testing.support.enabled env variable for integration test.

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/testingsupport/DeleteBookingControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialbooking/controller/testingsupport/DeleteBookingControllerIntegrationTest.java
@@ -2,10 +2,12 @@ package uk.gov.hmcts.reform.judicialbooking.controller.testingsupport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import uk.gov.hmcts.reform.judicialbooking.controller.BaseAuthorisedTestIntegration;
 
 import static uk.gov.hmcts.reform.judicialbooking.controller.utils.WiremockFixtures.ACTOR_ID1;
 
+@TestPropertySource(properties = {"testing.support.enabled=true"})
 public class DeleteBookingControllerIntegrationTest extends BaseAuthorisedTestIntegration {
     private static final String URL = "/am/testing-support/bookings/";
 
