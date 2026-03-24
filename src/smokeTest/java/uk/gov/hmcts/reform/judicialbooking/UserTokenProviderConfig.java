109fd2a4603c3792d99688b70327a92597e1a260
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-03-24 15:04:56 +0000

    DTSAM-1211 Switch the jenkins builds to use azure hmctsprod rather than hmctspublic.

--- src/smokeTest/java/uk/gov/hmcts/reform/judicialbooking/UserTokenProviderConfig.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/judicialbooking/UserTokenProviderConfig.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import uk.gov.hmcts.befta.util.EnvironmentVariableUtils;
 import uk.gov.hmcts.reform.idam.client.models.TokenRequest;
 
+
 @Getter
 public class UserTokenProviderConfig {
 
@@ -36,6 +37,7 @@ public class UserTokenProviderConfig {
         scope = EnvironmentVariableUtils.getRequiredVariable("OPENID_SCOPE_VARIABLES");
     }
 
+    
     public TokenRequest prepareTokenRequest() {
 
         return new TokenRequest(
