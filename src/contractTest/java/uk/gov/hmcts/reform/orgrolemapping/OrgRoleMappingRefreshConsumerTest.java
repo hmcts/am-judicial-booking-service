073deb1bd7e74ac85d08295d9765d6f69f9b6c14
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-06-15 14:26:08 +0100

    DTSAM-1278 Swap PactFolder for the new import.

--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/OrgRoleMappingRefreshConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/OrgRoleMappingRefreshConsumerTest.java
@@ -8,7 +8,7 @@ import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import au.com.dius.pact.core.model.annotations.PactFolder;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Maps;
